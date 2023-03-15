2ffafb8637f9d8f6d87bd340f99b86d1864a4722
Juan <juaoose@users.noreply.github.com>
2023-03-15 14:53:44 -0500

    remove map code from speed overelay

--- streamelements/speed/speed.js
+++ streamelements/speed/speed.js
@@ -1,25 +1,6 @@
 const pullKey = "<YOUR_RTIRL_PULL_KEY>";
-const mapboxToken = "<YOUR_MAPBOX_AT>";
 
 var app;
-var map = L.map("map").setView([0, 0], 13);
-L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-  {
-    accessToken: mapboxToken,
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: "mapbox/streets-v11",
-    tileSize: 512,
-    zoomOffset: -1,
-
-    zoom: 13,
-  }
-).addTo(map);
-
-// Setting this through options does not seem to work
-map.removeControl(map.zoomControl);
-map.dragging.disable();
 
 addEventListener("load", onReady);
 
